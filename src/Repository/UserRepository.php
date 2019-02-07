c1e3e4a105153be45fd0a830fff09ffd41fee28b
cassi <biagioli.cassandra@gmail.com>
2019-02-07 13:51:04 +0100

    change number request books list page

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -19,6 +19,19 @@ class UserRepository extends ServiceEntityRepository
         parent::__construct($registry, User::class);
     }
 
+     /**
+      * @return User[] Returns an array of User objects
+      */
+    public function findByUserID($value)
+    {
+        return $this->createQueryBuilder('u')
+            ->andWhere('u.id = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+
     // /**
     //  * @return User[] Returns an array of User objects
     //  */
